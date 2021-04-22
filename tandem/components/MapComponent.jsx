a6f37c98801c39500af204c87684f9352ed2b400
DanStevensonCO <danstevensonco@gmail.com>
2021-04-22 12:11:31 +0100

    add map with polyline

--- tandem/components/MapComponent.jsx
+++ tandem/components/MapComponent.jsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker, Polyline} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function MapComponent() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+
+      <MapView style={styles.map} initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+      }}>
+        <Polyline
+          coordinates={[
+            { latitude: 37.8025259, longitude: -122.4351431 },
+            { latitude: 37.7896386, longitude: -122.421646 },
+            { latitude: 37.7665248, longitude: -122.4161628 },
+            { latitude: 37.7734153, longitude: -122.4577787 },
+            { latitude: 37.7948605, longitude: -122.4596065 },
+            { latitude: 37.8025259, longitude: -122.4351431 }
+          ]}
+          strokeColor="#FF0000" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeWidth={3}
+        />
+      </MapView>
     </View>
   );
 }
