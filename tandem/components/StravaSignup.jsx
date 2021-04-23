a4cf2b5feeaf2e7dec48f3acedba671746bb69f6
DanStevensonCO <danstevensonco@gmail.com>
2021-04-23 09:24:22 +0100

    add Strava test API request with test keys/config

--- tandem/components/StravaSignup.jsx
+++ tandem/components/StravaSignup.jsx
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
 import { Button, Text, View, StyleSheet } from "react-native";
+import stravaApi from 'strava-v3';
+
+// import { REACT_NATIVE_STRAVA_CLIENT_ID } from "react-native-dotenv"
+
+stravaApi.config({
+  "access_token": "5c8ed25f992a3db4a0a84e0da18f98f46ba1e90f",
+  "client_id": "64734",
+  "client_secret":"1c40a829591afa728f4d54ed7b2408e11b092d9c",
+  "redirect_uri": "localhost:19006"
+})
+
+const strava = new stravaApi.client("5c8ed25f992a3db4a0a84e0da18f98f46ba1e90f")
 
 export default class App extends Component {
+  // stravaLogin() {
+  //   console.log("inside stravaLogin")
+
+  //   strava.athlete.get({id: 29656122},function(err, payload, limits) {
+  //     if(!err) {
+  //         console.log(payload);
+  //     }
+  //     else {
+  //         console.log(err);
+  //     }
+  //   });
+  // }
+  
+    async stravaLogin() {
+      console.log("inside stravaLogin")
+      
+      try {
+        const payload = await strava.athlete.get()
+        // const payload = await strava.oauth.getRequestAccessURL()
+        console.log(payload)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
   render() {
+
     const { navigation } = this.props;
 
     return (
@@ -11,7 +49,7 @@ export default class App extends Component {
           color="#FF4500"
           title={"Signup with Strava"}
           style={styles.input}
-          onPress={() => navigation.navigate("ConfigureProfile")}
+          onPress={this.stravaLogin}
           //either send strava id to configureProfile then send to db
           //OR send to db on strava then fetch user id and pass to configureProfile then update db
         />
