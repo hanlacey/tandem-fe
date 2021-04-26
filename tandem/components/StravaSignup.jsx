ee35591cd8c54b0b18fefa34d0de744b93127fde
DanStevensonCO <danstevensonco@gmail.com>
2021-04-26 10:16:24 +0100

    add other app buttons to signup page

--- tandem/components/StravaSignup.jsx
+++ tandem/components/StravaSignup.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import { View, Button } from 'react-native';
+
 import * as WebBrowser from 'expo-web-browser';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
-import { Button, StyleSheet, View } from 'react-native';
+import axios from 'axios'
+
+import { REACT_APP_STRAVA_CLIENT_ID, REACT_APP_STRAVA_CLIENT_SECRET } from "@env"
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -12,15 +16,16 @@ const discovery = {
   revocationEndpoint: 'https://www.strava.com/oauth/deauthorize',
 };
 
-export default function App() {
+
+
+export default function App({navigation}) {
+  
   const [request, response, promptAsync] = useAuthRequest(
     {
-      clientId: '64734',
+      clientId: REACT_APP_STRAVA_CLIENT_ID,
       scopes: ['activity:read_all'],
       redirectUri: makeRedirectUri({
-        // For usage in bare and standalone
-        // the "redirect" must match your "Authorization Callback Domain" in the Strava dev console.
-        native: 'localhost:19006/oauthredirect',
+        native: 'https://northcoders-tandem.netlify.app',
       }),
     },
     discovery
@@ -28,59 +33,54 @@ export default function App() {
 
   React.useEffect(() => {
     if (response?.type === 'success') {
-      console.log(response)
       const { code } = response.params;
       getActivityData(code)
-    }
+      }
   }, [response]);
 
   const getActivityData = (code) => {
-    fetch(`https://www.strava.com/oauth/token?client_id=64734&client_secret=1c40a829591afa728f4d54ed7b2408e11b092d9c&code=${code}&grant_type=authorization_code`, { method: "POST" })
-      .then(response => {
-        console.log(response)
-        // const accessToken = response.access_token
 
-        // fetch(`https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}`)
-        //   .then(response => {
-        //     console.log(response)
-        //   })
+    const config = {
+      headers: { 
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods' : 'GET,PUT,POST',
+      }
+    }
+
+    axios.post(`https://www.strava.com/oauth/token?client_id=${REACT_APP_STRAVA_CLIENT_ID}&client_secret=${REACT_APP_STRAVA_CLIENT_SECRET}&code=${code}&grant_type=authorization_code`, {}, config)
+      .then((response) => {
+        console.log(response)
+        const { access_token } = response.data
+        axios.get(`https://www.strava.com/api/v3/athlete/activities?access_token=${access_token}`)
+          .then((response) => {
+            console.log(response)
+        })
       })
   }
 
   return (
-    <View style={styles.container}>
-    <Button
-      color="#FF4500"
-      title={"Signup with Strava"}
-      style={styles.input}
-      disabled={!request}
-      onPress={() => {
-        promptAsync();
-      }}
-    />
-     <Button
+    <View>
+        <Button
+          disabled={!request}
+          title="Login"
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      
+
+        <Button
+          color="#FF4500"
+          title={"Configure profile"}
+          onPress={() => navigation.navigate("ConfigureProfile")}
+        />
+      
+        <Button
           title={"View routes"}
-          style={styles.button}
           onPress={() => navigation.navigate("MapComponent")}
           color="#841584"
         />
-    </View>
+      </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 10,
-  },
-});
