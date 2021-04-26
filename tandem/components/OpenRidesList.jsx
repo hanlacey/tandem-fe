a733e5252395ded8c9f29910fe26fe42ca8181c3
hanlacey <hnnhlcy@gmail.com>
2021-04-26 09:42:30 +0100

    debug post-merge

--- tandem/components/OpenRidesList.jsx
+++ tandem/components/OpenRidesList.jsx
@@ -1,37 +1,42 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	TouchableOpacity,
+} from "react-native";
 import rideData from "../assets/rides";
 import RideCard from "./RideCard";
 
 export default function OpenRidesList({ route }) {
-  const { user, userBikeType, userDifficulty } = route.params;
-  const [rides, setRides] = useState(rideData);
-  const [rideBikeType, setRideBikeType] = useState(userBikeType);
-  const [rideDifficulty, setRideDifficulty] = useState(userDifficulty);
+	const { user, userBikeType, userDifficulty } = route.params;
+	const [rides, setRides] = useState(rideData);
+	const [rideBikeType, setRideBikeType] = useState(userBikeType);
+	const [rideDifficulty, setRideDifficulty] = useState(userDifficulty);
 
-  const list = () => {
-    return rides.map((ride) => {
-      return (
+	const list = () => {
+		return rides.map((ride) => {
+			return (
+				<View key={ride.ride_id}>
+					<RideCard ride={ride} />
+				</View>
+			);
+		});
+	};
 
-        <View key={ride.ride_id}>
-          <RideCard ride={ride} />
-        </View>
-
-      );
-    });
-  };
-  return <ScrollView style={styles.container}>{list()}</ScrollView>;
+	return <ScrollView style={styles.container}>{list()}</ScrollView>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: "5%",
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: "20",
-    marginVertical: "8",
-    marginHorizontal: "16",
-  },
+	container: {
+		flex: 1,
+		marginTop: "5%",
+	},
+	item: {
+		backgroundColor: "#f9c2ff",
+		padding: "20",
+		marginVertical: "8",
+		marginHorizontal: "16",
+	},
 });
--- tandem/components/SingleRide.jsx
+++ tandem/components/SingleRide.jsx
@@ -1,96 +1,95 @@
-
-import * as React from 'react';
-import { Card, Paragraph, TextInput } from 'react-native-paper';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import { Card, Paragraph, TextInput } from "react-native-paper";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import parseDate from "../utils/parseDate";
-const ride =
-{
-  author: "raofRides",
-  ride_date: 1612329163389,
-  route_data: "Manchester",
-  ride_type: "road",
-  title: "Manchester loop",
-  description: "anyone want to join me on a loop around manchester",
-  experience_level: "intermediate",
-  created_at: 1601324163389,
-  votes: 0
-}
-
-export default function SingleRide({route}) {
-  const [text, setText] = React.useState('');
-  const { ride } = route.params;
-  return (
-    <View>
-      <Card style={styles.container}>
-        <Card.Title title={ride.route_data} subtitle={parseDate(ride.ride_date)} />
-        {/* <Text style={styles.commentContainer}>By {ride.author}</Text> */}
-        <Card.Content>
-          {/* <Title>{ride.route_data}</Title> */}
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-        <Card.Content>
-          <Paragraph style={styles.rideType}>Ride Type: {ride.ride_type}</Paragraph>
-          <Paragraph style={styles.rideType}>Experience Level: {ride.experience_level}</Paragraph>
-          <Paragraph>{ride.description}</Paragraph>
-        </Card.Content>
-        <Card.Actions>
-          <TouchableOpacity style={styles.button}>
-            <Paragraph>Ok</Paragraph>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Paragraph>Cancel</Paragraph>
-          </TouchableOpacity>
-        </Card.Actions>
-
-
-      </Card>
-      <View style={styles.commentContainer}>
-        <Text >Make a comment</Text>
-        <TextInput
-          label="write.."
-          value={text}
-          style={styles.commentBox}
-          onChangeText={text => setText(text)}
-        />
-      </View>
-    </View>
-
-  )
+const ride = {
+	author: "raofRides",
+	ride_date: 1612329163389,
+	route_data: "Manchester",
+	ride_type: "road",
+	title: "Manchester loop",
+	description: "anyone want to join me on a loop around manchester",
+	experience_level: "intermediate",
+	created_at: 1601324163389,
+	votes: 0,
 };
 
-const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 30,
-    marginTop: 20,
-    marginVeritcal: 10
+export default function SingleRide({ route }) {
+	const [text, setText] = React.useState("");
+	const { ride } = route.params;
+	return (
+		<View>
+			<Card style={styles.container}>
+				<Card.Title
+					title={ride.route_data}
+					subtitle={parseDate(ride.ride_date)}
+				/>
+				{/* <Text style={styles.commentContainer}>By {ride.author}</Text> */}
+				<Card.Content>{/* <Title>{ride.route_data}</Title> */}</Card.Content>
+				<Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+				<Card.Content>
+					<Paragraph style={styles.rideType}>
+						Ride Type: {ride.ride_type}
+					</Paragraph>
+					<Paragraph style={styles.rideType}>
+						Experience Level: {ride.experience_level}
+					</Paragraph>
+					<Paragraph>{ride.description}</Paragraph>
+				</Card.Content>
+				<Card.Actions>
+					<TouchableOpacity style={styles.button}>
+						<Paragraph>Ok</Paragraph>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.button}>
+						<Paragraph>Cancel</Paragraph>
+					</TouchableOpacity>
+				</Card.Actions>
+			</Card>
+			<View style={styles.commentContainer}>
+				<Text>Make a comment</Text>
+				<TextInput
+					label="write.."
+					value={text}
+					style={styles.commentBox}
+					onChangeText={(text) => setText(text)}
+				/>
+			</View>
+		</View>
+	);
+}
 
-  },
-  commentContainer: {
-    marginTop: 20,
-    marginHorizontal: 30,
-  },
-  commentBox: {
-    height: 100
-  },
-  button: {
-    color: "white",
-    fontWeight: 'bold',
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 100,
-    marginHorizontal: 30,
-    borderRadius: 30,
-    backgroundColor: "#FF4500",
-    shadowColor: 'black',
-    shadowOpacity: .2,
-    shadowOffset: {
-      height: 1,
-      width: -2
-    },
-    elevation: 2
-  },
-  rideType: {
-    textDecorationLine: 'underline'
-  }
-})
+const styles = StyleSheet.create({
+	container: {
+		marginHorizontal: 30,
+		marginTop: 20,
+		marginVertical: 10,
+	},
+	commentContainer: {
+		marginTop: 20,
+		marginHorizontal: 30,
+	},
+	commentBox: {
+		height: 100,
+	},
+	button: {
+		color: "white",
+		fontWeight: "bold",
+		height: 35,
+		justifyContent: "center",
+		alignItems: "center",
+		width: 100,
+		marginHorizontal: 30,
+		borderRadius: 30,
+		backgroundColor: "#FF4500",
+		shadowColor: "black",
+		shadowOpacity: 0.2,
+		shadowOffset: {
+			height: 1,
+			width: -2,
+		},
+		elevation: 2,
+	},
+	rideType: {
+		textDecorationLine: "underline",
+	},
+});
