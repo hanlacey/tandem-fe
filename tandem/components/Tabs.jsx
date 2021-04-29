b908cceefc10f08dc6c5eb7b4fa2936cbf9f3004
hanlacey <hnnhlcy@gmail.com>
2021-04-29 12:19:47 +0100

    styling

--- tandem/components/Tabs.jsx
+++ tandem/components/Tabs.jsx
@@ -1,16 +1,17 @@
-import React from "react"
-import {StyleSheet, Text, View, Image, TouchableOpacity} from "react-native"
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import StravaSignup from "./StravaSignup"
-import RideCard from "./RideCard"
-import UserProfile from "./UserProfile"
-import OpenRidesList from "./OpenRidesList"
-import EventAttendees from "./EventAttendees"
-import SingleRide from "./SingleRide"
-import PostRide from "./PostRide"
+import StravaSignup from "./StravaSignup";
+import RideCard from "./RideCard";
+import UserProfile from "./UserProfile";
+import OpenRidesList from "./OpenRidesList";
+import EventAttendees from "./EventAttendees";
+import SingleRide from "./SingleRide";
+import PostRide from "./PostRide";
 
-const Tab = createBottomTabNavigator()
+const hardcodeProfile = () => <UserProfile username="raofRides" />;
+const Tab = createBottomTabNavigator();
 
 // const CustomTabBarButton = (props) => {
 // 	console.log(props),
@@ -39,21 +40,12 @@ const Tab = createBottomTabNavigator()
 export default function Tabs() {
 	return (
 		<Tab.Navigator
+			style={styles.navigator}
 			tabBarOptions={{
-				showLabel: false,
-				style: {
-					position: "absolute",
-					bottom: 4,
-					left: 10,
-					right: 10,
-					elevation: 0,
-					paddingBottom: 17,
-					backgroundColor: "#ffffff",
-					borderRadius: 15,
-					height: 65,
-					...styles.shadow,
-				},
-			}}>
+				showLabel: true,
+				style: {},
+			}}
+		>
 			{/* <Tab.Screen
 				name="Home"
 				component={StravaSignup}
@@ -83,117 +75,129 @@ export default function Tabs() {
 				name="StravaSignup"
 				component={StravaSignup}
 				options={{
-					tabBarIcon: ({focused}) => (
-						<View
-							style={{alignItems: "center", justifyContent: "center", top: 10}}>
+					tabBarLabel: "Login",
+					tabBarIcon: ({ focused }) => (
+						<View style={styles.button}>
 							<Image
 								source={require("../assets/tab-icons/sign.png")}
 								resizeMode="contain"
 								style={{
-									width: 25,
-									height: 25,
+									width: 30,
+									height: 30,
 									tintColor: focused ? "#e32f45" : "#748c94",
 								}}
 							/>
-							<Text
-								style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>
+							{/* <Text
+								style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+							>
 								Sign up
-							</Text>
+							</Text> */}
 						</View>
 					),
 				}}
 			/>
 			<Tab.Screen
 				name="UserProfile"
-				component={UserProfile}
+				component={hardcodeProfile}
 				options={{
-					tabBarIcon: ({focused}) => (
-						<View
-							style={{alignItems: "center", justifyContent: "center", top: 10}}>
+					tabBarLabel: "Profile",
+
+					tabBarIcon: ({ focused }) => (
+						<View style={styles.button}>
 							<Image
 								source={require("../assets/tab-icons/user.png")}
 								resizeMode="contain"
 								style={{
-									width: 25,
-									height: 25,
+									width: 30,
+									height: 30,
 									tintColor: focused ? "#e32f45" : "#748c94",
 								}}
 							/>
-							<Text
-								style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>
+							{/* <Text
+								style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}
+							>
 								Profile
-							</Text>
+							</Text> */}
 						</View>
 					),
 				}}
 			/>
-			<Tab.Screen name="RideCard" component={RideCard} />
+			{/* <Tab.Screen name="RideCard" component={RideCard} /> */}
 			<Tab.Screen
-				name="PostRide"
+				name="Post a ride"
 				component={PostRide}
 				options={{
-					tabBarIcon: ({focused}) => (
-						<View
-							style={{alignItems: "center", justifyContent: "center", top: 10}}>
+					tabBarLabel: "New ride",
+
+					tabBarIcon: ({ focused }) => (
+						<View>
 							<Image
 								source={require("../assets/tab-icons/plus.png")}
 								resizeMode="contain"
 								style={{
-									width: 60,
-									height: 33,
+									width: 30,
+									height: 30,
 									tintColor: focused ? "#e32f45" : "#748c94",
 								}}
 							/>
-							<Text
-								style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>
-								Create
-					</Text>
+							{/* <Text
+								style={{
+									color: focused ? "#e32f45" : "#748c94",
+									fontSize: 12,
+								}}
+							>
+								New ride
+							</Text> */}
 						</View>
 					),
-					// tabBarButton: ( props ) => (
-					// <CustomTabBarButton { ...props }/>
-					// )
 				}}
 			/>
 			<Tab.Screen
 				name="OpenRidesList"
 				component={OpenRidesList}
 				options={{
-					tabBarIcon: ({focused}) => (
-						<View>
+					tabBarLabel: "Rides",
+
+					tabBarIcon: ({ focused }) => (
+						<View style={styles.button}>
 							<Image
 								source={require("../assets/tab-icons/bike.png")}
 								resizeMode="contain"
 								style={{
-									width: 25,
-									height: 25,
-									marginTop: 17,
+									width: 30,
+									height: 30,
 									tintColor: focused ? "#e32f45" : "#748c94",
 								}}
 							/>
-							<Text
-								style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>
+							{/* <Text
+								style={{
+									color: focused ? "#e32f45" : "#748c94",
+									fontSize: 12,
+								}}
+							>
 								Rides
-							</Text>
+							</Text> */}
 						</View>
 					),
 				}}
 			/>
-			<Tab.Screen name="Event Attendees" component={EventAttendees} />
-			<Tab.Screen name="SingleRide" component={SingleRide} />
+			{/* <Tab.Screen name="Event Attendees" component={EventAttendees} />
+			<Tab.Screen name="SingleRide" component={SingleRide} /> */}
 		</Tab.Navigator>
-	)
+	);
 }
 
 const styles = StyleSheet.create({
-	shadow: {
-		shadowColor: "#7F5DF0",
-		shadowOffset: {
-			width: 0,
-			height: 10,
-		},
-		shadowOpacity: 0.25,
-		shadowRadius: 3.5,
-		elevation: 5,
-	},
-})
+	navigator: { justifyContent: "space-evenly" },
+	button: { padding: 0 },
+	// shadow: {
+	// 	shadowColor: "#7F5DF0",
+	// 	shadowOffset: {
+	// 		width: 0,
+	// 		height: 10,
+	// 	},
+	// 	shadowOpacity: 0.25,
+	// 	shadowRadius: 3.5,
+	// 	elevation: 5,
+	// },
+});
